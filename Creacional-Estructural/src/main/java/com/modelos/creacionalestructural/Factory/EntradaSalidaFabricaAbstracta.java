62c87fe999de9ee114d354f1fb21ebd4e36f2946
SaiLord28 <nikomendi0728@gmail.com>
2025-03-27 23:16:17 -0500

    FabricaAbstracta Creada

---
+++ Creacional-Estructural/src/main/java/com/modelos/creacionalestructural/Factory/EntradaSalidaFabricaAbstracta.java
@@ -0,0 +1,9 @@
+package com.modelos.creacionalestructural.Factory;
+
+public interface EntradaSalidaFabricaAbstracta {
+
+    public Entrada crear_entrada();
+    public Salida crear_salida();
+
+
+}
---
+++ Creacional-Estructural/src/main/java/com/modelos/creacionalestructural/Factory/FabricaEntradaSalidaFrame.java
@@ -0,0 +1,9 @@
+package com.modelos.creacionalestructural.Factory;
+
+public interface FabricaEntradaSalidaFrame {
+
+    public Entrada crear_entrada();
+    public Salida crear_salida();
+
+
+}
