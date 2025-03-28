bbd88f949af519c559519aedd806c50edcc5dcab
SaiLord28 <nikomendi0728@gmail.com>
2025-03-27 23:19:46 -0500

    Arreglando Errores Fabrica Abstracta

--- Creacional-Estructural/src/main/java/com/modelos/creacionalestructural/Factory/EntradaSalidaFabricaAbstracta.java
+++ Creacional-Estructural/src/main/java/com/modelos/creacionalestructural/Factory/EntradaSalidaFabricaAbstracta.java
@@ -1,5 +1,8 @@
 package com.modelos.creacionalestructural.Factory;
 
+import com.modelos.creacionalestructural.entrada.Entrada;
+import com.modelos.creacionalestructural.salida.Salida;
+
 public interface EntradaSalidaFabricaAbstracta {
 
     public Entrada crear_entrada();
