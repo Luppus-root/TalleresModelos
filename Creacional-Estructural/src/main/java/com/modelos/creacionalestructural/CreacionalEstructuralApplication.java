a12a39cfe787e879a814bd8ba5c87af20f4e98b2
DSGS76 <duvansalinas1907@gmail.com>
2025-03-27 21:49:40 -0500

    Init

---
+++ Creacional-Estructural/src/main/java/com/modelos/creacionalestructural/CreacionalEstructuralApplication.java
@@ -0,0 +1,13 @@
+package com.modelos.creacionalestructural;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreacionalEstructuralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreacionalEstructuralApplication.class, args);
+	}
+
+}
---
+++ Creacional-Estructural/src/test/java/com/modelos/creacionalestructural/CreacionalEstructuralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modelos.creacionalestructural;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreacionalEstructuralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
