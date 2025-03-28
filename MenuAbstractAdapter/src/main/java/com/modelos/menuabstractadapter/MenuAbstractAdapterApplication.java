48759c37f123264660870678342c6ad44ea22360
DSGS76 <duvansalinas1907@gmail.com>
2025-03-27 20:46:51 -0500

    Init commit

---
+++ MenuAbstractAdapter/src/main/java/com/modelos/menuabstractadapter/MenuAbstractAdapterApplication.java
@@ -0,0 +1,13 @@
+package com.modelos.menuabstractadapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuAbstractAdapterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MenuAbstractAdapterApplication.class, args);
+    }
+
+}
