4a207ef02d3a805cfdc7bbcc5d4165d234c9f27c
DScode27 <davidsonsanchez1123@gmail.com>
2025-05-15 18:00:39 -0500

    Optimizar direcciones

---
+++ Estrategia/src/direccion/Direccion.java
@@ -0,0 +1,36 @@
+package direccion;
+
+public class Direccion {
+    public enum TIPOS_VIAS {
+        Carrera, Calle, Avenida, Diagonal, Transversal
+    }
+
+    public enum LOCALIDADES_BOGOTA {
+        Chapinero, Usaquén, SantaFe, Teusaquillo, LaCandelaria, SanCristóbal, Usme, Bosa, Kennedy, Fontibón
+    }
+
+    public enum PUNTOS_REFERENCIA {
+        frente_al_centro_comercial,
+        después_del_semáforo,
+        junto_al_parque,
+        cerca_del_hospital,
+        al_lado_de_la_estación_de_policía
+    }
+
+    public enum ATRACCIONES_TURISTICAS {
+        MONSERRATE,
+        MUSEO_DEL_ORO,
+        PLAZA_BOLIVAR,
+        MUSEO_BOTERO,
+        CERRO_DE_GUADALUPE,
+        PARQUE_DE_LA_93,
+        ZONA_T,
+        JARDIN_BOTANICO,
+        PLANETARIO,
+        MALOKA,
+        BIBLIOTECA_VIRGILIO_BARCO,
+        MUSEO_NACIONAL,
+        CENTRO_COMERCIAL_ANDINO,
+        PARQUE_SIMON_BOLIVAR
+    }
+}
