38954d6c2c02ef286cbf635a6635b990e15a517c
Luppus-root <mrguerreroc@udistrital.edu.co>
2025-06-26 20:10:50 -0500

    Proyecto final -BeautyAtHome

---
+++ BeautyAtHome/src/test/java/com/beautyathome/beautyathome/BeautyAtHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beautyathome.beautyathome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautyAtHomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
